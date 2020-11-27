91f161b4f17f149195eaf5126bc937d6efe3f149
bai <wubinbai@yahoo.com>
2020-11-27 21:05:29 +0800

    vlc in softwares

---
+++ softwares/vlc.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Install VLC Player for playing mp4
+
+sudo add-apt-repository ppa:videolan/master-daily
+sudo apt update
+
+# To install:
+
+sudo apt install vlc qtwayland5
+
+#In order to use the streaming and transcode features in VLC for Ubuntu 18.04, enter the following command to install the libavcodec-extra packages.
+
+sudo apt install libavcodec-extra
+
