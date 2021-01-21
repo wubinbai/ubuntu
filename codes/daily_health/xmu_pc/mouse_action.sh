2ec8837731d35cf3a7e34acc46b3159fde9b3c3e
wb <wubinbai@yahoo.com>
2021-01-21 12:07:48 +0800

    .

---
+++ codes/daily_health/xmu_pc/mouse_action.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+xdotool mousemove 421 124 click 1
+sleep 3.8
+xdotool mousemove 302 599 click 1
+sleep 3.8
+xdotool mousemove 1047 302 click 1
+sleep 4.8
+xdotool mousemove 1214 218 click 1
+sleep 3.5
+xdotool mousemove 687 519 click 1
+
