7c7d37f730d6b05b65409f4cb61a9642fe675e6b
wb <wubinbai@yahoo.com>
2021-01-22 11:36:05 +0800

    redo at 11

--- codes/daily_health/1024_768_xmu_pc/check_health_everyday.py
+++ codes/daily_health/1024_768_xmu_pc/check_health_everyday.py
@@ -6,12 +6,17 @@ MAX_ITER = int(1e99)
 NAP_HOUR = 0.2 # 0.2 hours
 NAP_SEC = int(NAP_HOUR * 60 * 60)
 DO_AT = 6 # 6 A.M.
-
+DO_AT2 = 11 # Recheck at 11 AM.
 
 ########## init dict
 d = dict()
 for i in range(1,366):
     d[i] = 0
+
+########## init dict2
+d2 = dict()
+for i in range(1,366):
+    d2[i] = 0
 
 for i in range(MAX_ITER):
     
@@ -28,5 +33,16 @@ for i in range(MAX_ITER):
                 end = time.time()
                 print('Done!', round(end - start,3), 'secs')
                 d[tm_yday] += 1
+    
+    if hour == DO_AT2: # if it is the time(correct hour) to do it
+        if d[tm_yday] < MAX_TRIALS: # if have not done for less than the number of trials
+            for num_trials in range(MAX_TRIALS):
+                os.system('date')
+                start = time.time()
+                os.system('./do_it.sh')
+                end = time.time()
+                print('Done!', round(end - start,3), 'secs')
+                d[tm_yday] += 1
+    
     
     time.sleep(NAP_SEC)
