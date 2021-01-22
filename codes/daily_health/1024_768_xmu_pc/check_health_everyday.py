e69d50240de4e56b11789e9fc392c7e9d9fe1f9c
wb <wubinbai@yahoo.com>
2021-01-22 11:43:42 +0800

    d2 in xmu daily check

--- codes/daily_health/1024_768_xmu_pc/check_health_everyday.py
+++ codes/daily_health/1024_768_xmu_pc/check_health_everyday.py
@@ -35,14 +35,14 @@ for i in range(MAX_ITER):
                 d[tm_yday] += 1
     
     if hour == DO_AT2: # if it is the time(correct hour) to do it
-        if d[tm_yday] < MAX_TRIALS + 1: # if have not done for less than the number of trials
+        if d2[tm_yday] == 0: # if have not done for less than the number of trials
             for num_trials in range(MAX_TRIALS):
                 os.system('date')
                 start = time.time()
                 os.system('./do_it.sh')
                 end = time.time()
-                print('Done!', round(end - start,3), 'secs')
-                d[tm_yday] += 1
+                print('Done!!', round(end - start,3), 'secs')
+                d2[tm_yday] += 1
     
     
     time.sleep(NAP_SEC)
